0cc34d45b75d2e92cabe85e2847a773b7a268c4a
Heriniaina <ramaheritoheriniaina@.com>
2026-03-19 20:13:44 +0300

    Feat : Added the rout GET/student

--- src/main/java/com/TD/spring_introduction/controller/StudentController.java
+++ src/main/java/com/TD/spring_introduction/controller/StudentController.java
@@ -1,9 +1,9 @@
 package com.TD.spring_introduction.controller;
 
 import com.TD.spring_introduction.model.Student;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,9 +16,22 @@ public class StudentController {
 
     @PostMapping("/student")
     public List<Student> addStudent(@RequestBody List<Student> newStudents) {
-
         studentsMemory.addAll(newStudents);
-
         return studentsMemory;
+    }
+
+    @GetMapping("/student")
+    public ResponseEntity<?> getStudentsNames(@RequestHeader(value = "Accept", defaultValue = "text/plain") String acceptHeader){
+        if ("text/plain".equalsIgnoreCase(acceptHeader)){
+            String names = studentsMemory
+                    .stream()
+                    .map(Student::getName)
+                    .collect(Collectors.joining(", "));
+            return ResponseEntity.ok(names);
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.NOT_ACCEPTABLE)
+                    .body("Format not supported");
+        }
     }
 }
