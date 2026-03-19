f6c5618334f4567b04eb9b311bb745b477cde3bf
Heriniaina <ramaheritoheriniaina@.com>
2026-03-19 16:13:50 +0300

    feat: add WelcomeController for GET /welcome route

---
+++ src/main/java/com/TD/spring_introduction/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.TD.spring_introduction;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/welcome")
+    public String welcome(
+            @RequestParam (name = "name", defaultValue = "Guest") String name){
+    return "Welcome " + name;
+    }
+}
